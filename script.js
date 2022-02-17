5d89087213facbfe29caadddf321c97601642aed
susccrua <susccrua@gmail.com>
2022-02-17 11:33:34 -0600

    Tweaks to header, responsiveness

--- script.js
+++ script.js
@@ -1,47 +1,48 @@
+const pboxes = document.getElementsByClassName("pbox");
+const titles = document.getElementsByClassName("title");
+
 const picApp = document.getElementsByClassName("picApp");
 const livingAtlas = document.getElementsByClassName("livingAtlas");
 const macMeals = document.getElementsByClassName("macMeals");
 const jsExercises = document.getElementsByClassName("jsExercises");
+console.log(picApp);
 
-const pboxes = document.getElementsByClassName("pbox");
-const titles = document.getElementsByClassName("title");
-
-picApp[1].onmouseenter = () => {
+picApp[0].onmouseenter = () => {
   for (let i = 0; i < pboxes.length; i++) {
-    console.log(i);
+    console.log(this);
     pboxes[i].classList.add("inactive");
     titles[i].classList.add("inactive");
   }
-  picApp[0].classList.remove("inactive");
   picApp[1].classList.remove("inactive");
+  picApp[0].classList.remove("inactive");
 };
 
-livingAtlas[1].onmouseenter = () => {
+livingAtlas[0].onmouseenter = () => {
   for (let i = 0; i < pboxes.length; i++) {
     console.log(i);
     pboxes[i].classList.add("inactive");
     titles[i].classList.add("inactive");
   }
-  livingAtlas[0].classList.remove("inactive");
   livingAtlas[1].classList.remove("inactive");
+  livingAtlas[0].classList.remove("inactive");
 };
 
-macMeals[1].onmouseenter = () => {
+macMeals[0].onmouseenter = () => {
   for (let i = 0; i < pboxes.length; i++) {
     console.log(i);
     pboxes[i].classList.add("inactive");
     titles[i].classList.add("inactive");
   }
-  macMeals[0].classList.remove("inactive");
   macMeals[1].classList.remove("inactive");
+  macMeals[0].classList.remove("inactive");
 };
 
-jsExercises[1].onmouseenter = () => {
+jsExercises[0].onmouseenter = () => {
   for (let i = 0; i < pboxes.length; i++) {
     console.log(i);
     pboxes[i].classList.add("inactive");
     titles[i].classList.add("inactive");
   }
-  jsExercises[0].classList.remove("inactive");
   jsExercises[1].classList.remove("inactive");
+  jsExercises[0].classList.remove("inactive");
 };
